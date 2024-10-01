adf7936ec9f80428ba6d5faaa3bf8d698cb5c9df
monzur rahman <89568953+monzurrahman@users.noreply.github.com>
2024-10-01 15:57:32 +0600

    Summation of some give number code added

--- Largest-number.php
+++ Largest-number.php
@@ -10,3 +10,11 @@ for ($i =0; $i<(sizeof($arr)-1); $i++){
    else $largest = $arr[$i+1];
 }
 echo $largest;
+
+// Get the summation of some given numbers like: 2,5,1,10
+/* $arr = array(2,5,1,10);
+$sum=0;
+for ($i =0; $i<sizeof($arr); $i++){
+   $sum = $sum + $arr[$i];
+}
+echo $sum; */
