6ef2b8675711cf432d0e02ee5942dfb2c3f5b213
monzur rahman <89568953+monzurrahman@users.noreply.github.com>
2024-10-01 15:28:45 +0600

    Update Largest-number.php
    No need to initialize variable

--- Largest-number.php
+++ Largest-number.php
@@ -1,7 +1,7 @@
 <?php
 // find the largest number from some given numbers like: 2,5,1,10
 $arr = array(2,5,1,10);
-$largest=0;
+$largest;
 for ($i =0; $i<(sizeof($arr)-1); $i++){
    if($arr[$i]>$arr[$i+1]){
        
